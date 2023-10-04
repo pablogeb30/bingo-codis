57bc14f2a74c4c3bd7bf70b6449d61330ed18f06
pablogeb30 <pagebas@me.com>
2023-10-04 13:49:48 +0200

    Estructura cliente servidor hecha

--- bingo-codis/src/main/java/pablo/Cliente.java
+++ bingo-codis/src/main/java/pablo/Cliente.java
@@ -1,10 +1,16 @@
 package pablo;
 
-import java.util.ArrayList;
+import java.net.*;
+import java.util.*;
 
 public class Cliente {
 
+    // Constantes
+    private static final String IP = "225.0.0.100";
+    private static final int PUERTO = 6789;
+
     private ArrayList<Integer> carton;
+    private boolean hayBingo = false;
 
     public Cliente() {
         this.carton = new ArrayList<Integer>();
@@ -12,8 +18,9 @@ public class Cliente {
 
     public void generarCarton() {
         int cont = 0;
+        Random random = new Random();
         while (cont < 15) {
-            int num = (int) (Math.random() * 90 + 1);
+            int num = random.nextInt(90) + 1;
             if (!carton.contains(num)) {
                 carton.add(num);
                 cont++;
@@ -25,7 +32,7 @@ public class Cliente {
         System.out.println("Carton:");
         for (int i = 0; i < carton.size(); i++) {
             if (carton.get(i) < 10) {
-                System.out.print(" ");
+                System.out.print("0");
             }
             System.out.print(carton.get(i) + " ");
             if ((i + 1) % 5 == 0) {
@@ -33,6 +40,45 @@ public class Cliente {
             }
         }
         System.out.println();
+    }
+
+    public static void main(String[] args) {
+        MulticastSocket s = null;
+        try {
+            InetAddress group = InetAddress.getByName(IP);
+            s = new MulticastSocket(PUERTO);
+            s.joinGroup(group);
+
+            Cliente cliente = new Cliente();
+            cliente.generarCarton();
+            cliente.imprimirCarton();
+
+            while (cliente.hayBingo == false) {
+                byte[] buffer = new byte[2];
+                DatagramPacket paquete = new DatagramPacket(buffer, buffer.length);
+                s.receive(paquete);
+                int bola = Integer.parseInt(new String(paquete.getData()));
+                System.out.println("Ha salido la bola: " + bola + "\n");
+
+                if (cliente.carton.contains(bola)) {
+                    cliente.carton.remove(cliente.carton.indexOf(bola));
+                }
+
+                if (cliente.carton.isEmpty()) {
+                    cliente.hayBingo = true;
+                }
+
+            }
+
+            System.out.println("BINGO!");
+
+            s.leaveGroup(group);
+            s.close();
+            System.out.println("Fin del juego.\n");
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
 }
--- bingo-codis/src/main/java/pablo/Servidor.java
+++ bingo-codis/src/main/java/pablo/Servidor.java
@@ -1,8 +1,13 @@
 package pablo;
 
-import java.util.ArrayList;
+import java.net.*;
+import java.util.*;
 
 public class Servidor {
+
+    // Constantes
+    private static final String IP = "225.0.0.100";
+    private static final int PUERTO = 6789;
 
     private ArrayList<Integer> bombo;
 
@@ -16,8 +21,8 @@ public class Servidor {
         }
     }
 
-    public int sacarBola() {
-        int bola = (int) (Math.random() * bombo.size());
+    public int sacarBola(Random random) {
+        int bola = random.nextInt(90) + 1;
         int num = bombo.get(bola);
         bombo.remove(bola);
         return num;
@@ -27,7 +32,7 @@ public class Servidor {
         System.out.println("Bombo:");
         for (int i = 0; i < bombo.size(); i++) {
             if (bombo.get(i) < 10) {
-                System.out.print(" ");
+                System.out.print("0");
             }
             System.out.print(bombo.get(i) + " ");
             if ((i + 1) % 10 == 0) {
@@ -35,6 +40,37 @@ public class Servidor {
             }
         }
         System.out.println();
+    }
+
+    public static void main(String[] args) {
+        MulticastSocket s = null;
+        try {
+            InetAddress group = InetAddress.getByName(IP);
+            s = new MulticastSocket(PUERTO);
+            s.joinGroup(group);
+
+            Servidor servidor = new Servidor();
+            Random random = new Random();
+            servidor.generarBombo();
+            servidor.imprimirBombo();
+
+            for (int i = 0; i < 90; i++) {
+                int bola = servidor.sacarBola(random);
+                String mensaje = String.format("%02d", bola);
+                byte[] buffer = mensaje.getBytes();
+                DatagramPacket paquete = new DatagramPacket(buffer, buffer.length, group, PUERTO);
+                s.send(paquete);
+                System.out.println("Ha salido la bola: " + bola + "\n");
+                Thread.sleep(1000);
+            }
+
+            s.leaveGroup(group);
+            s.close();
+            System.out.println("Fin del juego.\n");
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
 }
